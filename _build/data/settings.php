81ae4a58f3f4eed668baefa3f47212a571337763
digitalpenguin <murray@digitalpenguin.hk>
2021-01-26 13:57:06 +0800

    Added path settings to build script

--- _build/data/settings.php
+++ _build/data/settings.php
@@ -1,6 +1,18 @@
 <?php
 
 return [
+    'assets_path' => [
+        'area' => 'Paths',
+        'value' => '{assets_path}components/commerce_imagemeld/',
+    ],
+    'assets_url' => [
+        'area' => 'Paths',
+        'value' => '{assets_url}components/commerce_imagemeld/',
+    ],
+    'core_path' => [
+        'area' => 'Paths',
+        'value' => '{core_path}components/commerce_imagemeld/',
+    ],
     'melds_path' => [
         'area' => 'Paths',
         'value' => '{assets_path}imagemelds/',
