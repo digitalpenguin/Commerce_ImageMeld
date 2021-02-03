78a6f53e3f93fa36e954ab0b011001d22f437c0d
digitalpenguin <murray@digitalpenguin.hk>
2021-02-03 15:03:36 +0800

    Added French lexicon

---
+++ core/components/commerce_imagemeld/lexicon/fr/default.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$_lang['commerce_imagemeld'] = 'Commerce_ImageMeld';
+$_lang['commerce_imagemeld.description'] = 'custom design et envoie d\'image par le client.';
+
+$_lang['commerce_imagemeld.admin.melded_image'] = 'Image composée';
+$_lang['commerce_imagemeld.admin.uploaded_image'] = 'Image uploadée';
+$_lang['commerce_imagemeld.admin.custom_design'] = 'Custom Design - Images jointes';
+$_lang['commerce_imagemeld.admin.download'] = 'Download';
+
+$_lang['commerce_imagemeld.form.save'] = 'Valider';
+
+$_lang['commerce_imagemeld.error.missing_params'] = 'Nous n\'avons pas pu vérifier les données téléchargées. Veuillez réessayer.';
+$_lang['commerce_imagemeld.error.invalid_image_type'] = 'Type d\'image invalide. L\'image doit être au format PNG ou JPG.';
+$_lang['commerce_imagemeld.error.image_too_small'] = 'Désolé, la taille de l\'image est trop petite.';
+$_lang['commerce_imagemeld.error'] = 'Quelque chose s\'est mal passé avec le téléchargement.';
